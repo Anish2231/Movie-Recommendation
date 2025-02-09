7ab0d82c06a19f34ae374923dd34f25728b7a1b1
Anish Patil <137384437+Anish2231@users.noreply.github.com>
2025-02-09 15:55:05 +0530

    Update firebaseConfig.jsx

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; 
 import { getFirestore } from "firebase/firestore";   
 const firebaseConfig = {
-    apiKey: "AIzaSyDXfvaLSLNdP5mazuujk-yqY-WanZ3mlZo",
-    authDomain: "movie-recommendation-7ebc1.firebaseapp.com",
-    projectId: "movie-recommendation-7ebc1",
-    storageBucket: "movie-recommendation-7ebc1.appspot.com", // Corrected storageBucket
-    messagingSenderId: "69865121383",
-    appId: "1:69865121383:web:653d63e7f567ea966b96c2"
+  
 };
 
 const app = initializeApp(firebaseConfig);
